8fe25f5fc8b14de3957c6e6e095135db98e8fa6e
Abrar Noman <kingabrar1@gmail.com>
2024-11-22 17:31:14 -0500

    Updated the Hashed passwords when Registering and logging in

--- HealthQuest/backend/src/controllers/authController.js
+++ HealthQuest/backend/src/controllers/authController.js
@@ -14,11 +14,8 @@ const registerUser = async (req, res) => {
             return res.status(400).json({ message: 'User already exists' });
         }
 
-        // Hash password
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        // Create user
-        const user = await User.create({ name, email, password: hashedPassword });
+        // Create user (hashing is handled by the schema middleware)
+        const user = await User.create({ name, email, password });
 
         // Generate token
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
@@ -28,6 +25,7 @@ const registerUser = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
 
 // Login user
 const loginUser = async (req, res) => {
@@ -40,7 +38,8 @@ const loginUser = async (req, res) => {
             return res.status(404).json({ message: 'User not found' });
         }
 
-        console.log('User found:', user);
+        console.log('Password from request:', password);
+        console.log('Hashed password from DB:', user.password);
 
         const isMatch = await bcrypt.compare(password, user.password);
         console.log('Password match result:', isMatch);
@@ -63,6 +62,7 @@ const loginUser = async (req, res) => {
         res.status(500).json({ message: 'Server error', error: error.message });
     }
 };
+
 
 
 module.exports = { registerUser, loginUser };
