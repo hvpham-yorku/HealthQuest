9ac38ba743d674df2fa80f345e7a090eb7cb27fe
Humza987 <humzainam@gmail.com>
2024-11-27 12:19:27 -0500

    Sprint 2 Task 25 - Redesign Header

--- HealthQuest/frontend/src/components/Header.js
+++ HealthQuest/frontend/src/components/Header.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import './Header.css'; // Importing a separate CSS file for the Header component
 
 function Header() {
-  return <h1>HealthQuest</h1>;
+  return (
+    <div className="header-container">
+      <h1 className="title">HealthQuest</h1>
+    </div>
+  );
 }
 
 export default Header;
