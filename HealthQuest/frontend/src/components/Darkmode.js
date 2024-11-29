b74906de26059daf37edee88484209bc52e90b16
Luckman21 <luckmaan21@gmail.com>
2024-11-29 05:52:25 -0500

    Change Webpage Background Colour
    Changed the webpage background colour with this line.
    
    TODO: Create an external function that listens to the switch.  When triggered, it sets colours to all the elements of the webpage.

--- HealthQuest/frontend/src/components/Darkmode.js
+++ HealthQuest/frontend/src/components/Darkmode.js
@@ -4,7 +4,8 @@ import "./Darkmode.css"
 var darkmode = false;
 
 const DarkSwitch = () => {
-    darkmode = !darkmode;
+    document.body.style.backgroundColor = !(darkmode) ? "#add8e6":"#355393" // Change colour of the webpage background
+    //document.getElementById("ElementId").style.backgroundColor= !(darkmode) ? "#111111":"#000000"
     return (
         <label className="darkswitch">
             <input type="checkbox" />
